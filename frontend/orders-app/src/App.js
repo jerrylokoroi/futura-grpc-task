fe3f62e74b24e142948f8256f06fd35d28805cd6
Ekuwam Lokoroi <j.ekuwam@futura-solutions.de>
2026-05-28 16:33:09 +0200

    feat: update image names to Docker Hub and imagePullPolicy to Always

--- frontend/orders-app/src/App.js
+++ frontend/orders-app/src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const API_URL = process.env.REACT_APP_API_URL || "";
+const API_URL = process = "";
 function App() {
   const [orders, setOrders] = useState([]);
   const [customerName, setCustomerName] = useState("");
